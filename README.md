e03debab664af6de5400719a0c5214f9280d04bd
scarlett07 <scarlett.geo@gmail.com>
2017-12-04 10:26:57 -0600

    agragando el readme y las modificaciones para mostrar el tuit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Twitter
+
+Version  06/12/17
+
+La esta página, puedes postear ideas o pensamientos rapidos y cortos, conun limite de caracteres de 140, si execes este limite, no podras postear nada.
+
+Su estructura
--- js/app.js
+++ js/app.js
@@ -3,21 +3,35 @@ document.getElementById('tweet').addEventListener('keyup',availableCharacters);
 
 function showTweet() {
 	document.getElementById('tweets').style.display = 'block';
+	//obtenemos el mensaje que escribio el usuario
 	var tweet = document.getElementById('tweet').value;
-	//estas son los nuevos elementos a crear
-	var contenedorTuit = document.createElement('article');
-	//segun el cuadro de inspeccionar, esta variable es la que no esta definida, si era un <p> y para eso se ocupa???
-	var tiut = document.createElement('p');
-	tiut.innerHTML = tweet;
-	// para decirle decirle donde lo va a colocar
-	tuit.appendChild(contenedorTuit);
-	contenedorTuit.appendChild(document.getElementById('tweets'));
+	//creamos el espacio donde lo colocaremos
+	var pTweet = document.createElement('p');
+	//metemos el mensaje en el lugar y le damos el estilo con una clase.
+	pTweet.className = "tweet-content";
+	pTweet.innerHTML = tweet;
+	//creamos las variables que nos faltan, para que quede como en el html.
+	var tweetContainer = document.createElement('article');
+	tweetContainer.className = "tweet";
+	//le decimos donde colocar estos nuevos elementos
+	tweetContainer.appendChild(pTweet);
+	//console.log(tweetContainer);
+	document.getElementById("tweets").appendChild(tweetContainer);
+	document.getElementById('tweet').value = "";
+	console.log(document.getElementById('tweets'));
 }
 
 function availableCharacters() {
+	//obtenemos el mensaje donde se contaran los carácteres.
 	var tweet = document.getElementById('tweet').value;
+	//creamos la variable donde se guardará el nuemero de caracteres que el usuarrio sobrepaso.
 	var availableCharacters = 140 - tweet.length;
 	document.getElementById('submit-tweet').disabled = tweet.length > 0 ? false : true;
 	document.getElementById('available-characters').innerHTML = availableCharacters;
-
+	document.getElementById('submit-tweet').disabled = availableCharacters <= 0 ? true : false;
+	if(availableCharacters <= 20 && availableCharacters > 10) {
+		document.getElementById('available-characters').style.color = 'red';
+	} else if (availableCharacters <= 10) {
+		document.getElementById('available-characters').style.color = 'blue';
+	}
 }
